306f56eb83abe7ef25a8fb512a560b6539016567
Prince Raj <princeraj427@gmai.com>
2019-10-03 18:12:12 +0530

    custom toolbar added

--- app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
+++ app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
@@ -3,12 +3,21 @@ package com.example.prince.yourtaskapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Your Task App");
     }
 }
