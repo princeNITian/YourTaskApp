3bf83cc4fdaafea63facaa74b5ac6c1bb4ce72b6
Prince Raj <princeraj427@gmai.com>
2019-10-04 12:58:22 +0530

    Insert to firebase realtime database successful

--- app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
+++ app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 //import android.widget.Toolbar;
+import com.example.prince.yourtaskapp.Model.Data;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -16,6 +28,11 @@ public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FloatingActionButton fabBtn;
+
+    // Firebase
+    private DatabaseReference mDatabase;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,11 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Your Task App");
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("TaskNote");
+
         fabBtn = findViewById(R.id.fab_btn);
 
         fabBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +57,39 @@ public class HomeActivity extends AppCompatActivity {
                 LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
                 View myView = inflater.inflate(R.layout.custominputfield,null);
                 myDialog.setView(myView);
-                AlertDialog dialog = myDialog.create();
+                final AlertDialog dialog = myDialog.create();
+
+                final EditText title = myView.findViewById(R.id.editTitle);
+                final EditText note = myView.findViewById(R.id.editNote);
+
+                Button btnSave = myView.findViewById(R.id.btnSave);
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if(TextUtils.isEmpty(mTitle)){
+                            title.setError("Required field..");
+                            return;
+                        }
+                        if(TextUtils.isEmpty(mNote)){
+                            note.setError("Required field..");
+                            return;
+                        }
+
+                        String id = mDatabase.push().getKey();
+                        String date = DateFormat.getDateInstance().format(new Date());
+
+                        Data data = new Data(mTitle,mNote,date,id);
+
+                        mDatabase.child(id).setValue(data);
+                        Toast.makeText(getApplicationContext(),"Data Insert",Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
                 dialog.show();
 
 
