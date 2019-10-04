e68c0f184ed5a8635d945514d91a8671c345498e
Prince Raj <princeraj427@gmai.com>
2019-10-04 09:55:21 +0530

    custom fab icon added also data model created

--- app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
+++ app/src/main/java/com/example/prince/yourtaskapp/HomeActivity.java
@@ -3,13 +3,17 @@ package com.example.prince.yourtaskapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 //import android.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import androidx.appcompat.widget.Toolbar;
 
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FloatingActionButton fabBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,14 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_home);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Your Task App");
+
+        fabBtn = findViewById(R.id.fab_btn);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
